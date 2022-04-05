3529cbf79ae040050be88915ad6bda8417536e1b
Fernando Benavides <nockzblack@gmail.com>
2022-04-05 09:46:10 -0500

    Initial Commit

---
+++ Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by Fernando's Mac on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
